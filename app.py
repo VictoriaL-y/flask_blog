e91961f7a5320f3ab988670d1375a7ac0525dee1
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2022-07-25 21:23:34 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import sqlite3 
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from forms import RegistrationForm, LoginForm
+
+@app.route("/")
+@app.route("/home")
+def home():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close() 
+    return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
